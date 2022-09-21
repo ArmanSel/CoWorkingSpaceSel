2932dfc975d08bf9a4b99034c5d21f94e7475aaa
Defu5er <96230050+ArmanSel@users.noreply.github.com>
2022-09-21 14:34:48 +0200

    Added delete client function
    A client can now delete itself.

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO BOOKING (id, name, booking_type, status)
-VALUES ('413e2297-b84b-42ef-97ed-16a8a9d1d671', 'Jesse Meier', 'DAY', 'APPROVED'),
-       ('b8160463-01a0-4c7a-bd46-5b3716dbe4c6', 'Gus Gusman','AFTERNOON', 'APPROVED'),
+VALUES ('413e2297-b84b-42ef-97ed-16a8a9d1d671', 'Jesse Meier', 'DAY', 'PENDING'),
+       ('b8160463-01a0-4c7a-bd46-5b3716dbe4c6', 'Gus Gusman','AFTERNOON', 'PENDING'),
        ('3c13c533-fbac-4881-b94d-f95cb2ef16c8', 'Jesse Meier','MORNING', 'APPROVED');
 
 INSERT INTO MEMBER (id, username, email, firstname, lastname, password_hash, newsletter, is_admin)
